f86d42b458ceae4f33c01d7f747ee214a9095253
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-06-29 18:11:01 +0200

    Add Polumorphism

--- OOP.py
+++ OOP.py
@@ -19,3 +19,25 @@ s1=  SciCalculator('mahmoud')
 s1.sum(2,3)
 s1.mul(4,5)
 s1.power(2,5)
+
+
+#Polumorphism
+
+class A :
+    def do(self):
+        print('in A')
+
+class B (A):
+    pass
+
+class C :
+    def do(self):
+        print('in C')
+
+class D(B,C) :
+    pass
+
+
+p1=D()
+print(D.mro())
+p1.do()
